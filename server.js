41b13fd9839874a23a36874a8731724c604b82d8
John Malhinha <john.malhinha@gmail.com>
2021-06-09 19:17:20 -0700

    Magic 8 Ball express application created.

--- server.js
+++ server.js
@@ -6,6 +6,32 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+/*********************
+DATA MODEL
+**********************/
+
+const responses = [
+  "It is certain",
+  "It is decidedly so",
+  "Without a doubt",
+  "Yes definitely",
+  "You may rely on it",
+  "As I see it yes",
+  "Most likely",
+  "Outlook good",
+  "Yes",
+  "Signs point to yes",
+  "Reply hazy try again",
+  "Ask again later",
+  "Better not tell you now",
+  "Cannot predict now",
+  "Concentrate and ask again",
+  "Don't count on it",
+  "My reply is no",
+  "My sources say no",
+  "Outlook not so good",
+  "Very doubtful"
+];
 
 /*********************
 INDUCE ROUTES
@@ -22,9 +48,13 @@ app.get('/greeting/:name', (req, res) => {
 app.get('/tip/:total/:tipPercentage', (req, res) => {
   const tipTotal = (parseInt(req.params.total, 10))*((parseInt(req.params.tipPercentage, 10))/100);
   res.send(`${tipTotal}`);
+});
 
-  // const totalInt = parseInt(req.params.total, 10);
-  // console.log(totalInt, typeof totalInt);
+// Magic 8 Ball
+
+app.get('/magic/:phrase', (req, res) => {
+  const answer = Math.floor(Math.random() * responses.length);
+  res.send(`<h1>${responses[answer]}</h1>`);
 });
 
 
